2250a3f487c4ae98b180de9792833a6d3346178f
naoya_muto <naoya_muto@r.recruit.co.jp>
2017-03-14 07:52:37 +0900

    Success to install service worker in localhost!

--- service-worker.js
+++ service-worker.js
@@ -7,6 +7,7 @@ var urlsToCache = [
 
 self.addEventListener('install', function(event) {
   // インストール処理
+  debugger
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
@@ -14,4 +15,42 @@ self.addEventListener('install', function(event) {
         return cache.addAll(urlsToCache);
       })
   );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // キャッシュがあったのでレスポンスを返す
+        if (response) {
+          return response;
+        }
+
+        // 重要：リクエストをcloneする。リクエストはStreamなので
+        // 一度しか処理できない。ここではキャッシュ用、fetch用と2回
+        // 必要なので、リクエストはcloneしないといけない
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // レスポンスが正しいかをチェック
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // 重要：レスポンスをclone する。レスポンスはStreamで
+            // ブラウザ用とキャッシュ用の2回必要。なのでcloneして
+            // 2つのStreamがあるようにする
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
